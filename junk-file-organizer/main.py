9210a4ce999c22428741115b4169621592b2b83e
Mintu Krishnan S <mintu.krish999@hotmail.com>
2020-11-23 17:26:20 +0530

    trial

---
+++ junk-file-organizer/main.py
@@ -0,0 +1,47 @@
+from directories import *
+
+import os
+
+from pathlib import Path
+
+FILE_FORMATS = {
+    file_format: directory
+    for directory, file_formats in DIRECTORIES.items()
+    for file_format in file_formats
+}
+
+
+def organize_junk():
+
+    for entry in os.scandir():
+
+        if entry.is_dir():
+
+            continue
+
+        file_path = Path(entry)
+
+        file_format = file_path.suffix.lower()
+
+        if file_format in FILE_FORMATS:
+
+            directory_path = Path(FILE_FORMATS[file_format])
+
+            directory_path.mkdir(exist_ok=True)
+
+            file_path.rename(directory_path.joinpath(file_path))
+
+        for dir in os.scandir():
+
+            try:
+
+                os.rmdir(dir)
+
+            except:
+
+                pass
+
+
+if __name__ == "__main__":
+
+    organize_junk()
